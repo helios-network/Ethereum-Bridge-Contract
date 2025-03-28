1d057239f01626c4a82c1de0da0e990d68029d77
jguyet <jguyet.weoohh@gmail.com>
2025-03-28 22:55:10 +0100

    deploy ahelios

---
+++ scripts/deploy-erc20-ahelios.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    const HyperionAddress = "0x27ac383Cac26866F94e905C5Ab135b4Ab669c936";
+    const hyperionContract = (await hre.ethers.getContractFactory("Hyperion")).attach(HyperionAddress);
+
+    const HELIOS_ERC20_DENOM="ahelios"
+    const ERC20_NAME="Helios"
+    const ERC20_SYMBOL="HLS"
+    const ERC20_DECIMALS=18
+
+    const result = await hyperionContract.deployERC20(HELIOS_ERC20_DENOM, ERC20_NAME, ERC20_SYMBOL, ERC20_DECIMALS);
+
+    console.log(result);
+
+    console.log(await result.wait());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
